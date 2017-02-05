92e0e22b5c5273d972c18c4aa0d011ddb55469a2
LRShao <LRShao@users.noreply.github.com>
2017-02-04 23:03:38 -0500

    Add code for 1046(*)

---
+++ 1046-Shortest-Distance.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define MAX_NUMBER_OF_EXITS 100001
+
+// in order to avoid timeout, store the accumulative distance instead of interval distance
+
+int main() {
+    int N;
+    long int accumulativeDistance[ MAX_NUMBER_OF_EXITS ] = {0};
+    
+    scanf("%d", &N);
+    
+    long int D;
+    for( int i = 1; i <= N; i++ ) {
+        scanf(" %ld", &D );
+        accumulativeDistance[ i ] += accumulativeDistance[ i-1 ] + D;
+    }
+
+    int M;
+    scanf("\n%d", &M );
+
+    int E1, E2;
+    long int totalDistance = accumulativeDistance[ N ];
+    long int onewayDistance;
+    for( int j = 1; j <= M; j++ ) {
+        scanf("\n%d %d", &E1, &E2 );
+        // decrease runtime
+
+        if ( E2 < E1 ) {
+            int t  = E1;
+            E1 = E2;
+            E2 = t;
+        }
+
+        onewayDistance = accumulativeDistance[ --E2 ] - accumulativeDistance[ --E1 ];
+        
+        if ( 2 * onewayDistance > totalDistance )
+            onewayDistance = totalDistance - onewayDistance;
+        
+        printf("%ld\n", onewayDistance );
+    }
+
+    return 0;
+}
